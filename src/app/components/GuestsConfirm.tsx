7dc5d11f093167449a3165194b0931b98ae0298e
SamuelBarbosa1 <samuelbarbosa2001oliveira@gmail.com>
2024-06-22 17:31:39 -0300

    fixed

--- src/app/components/GuestsConfirm.tsx
+++ src/app/components/GuestsConfirm.tsx
@@ -8,22 +8,45 @@ import saveData from "../services/firebase";
 
 export default function GuestsConfirm() {
   const [modalOpen, setModalOpen] = useState(false);
-  const [displayShowsSize, setDisplayShowsSize] = useState(false);
+  const [numberOfPeople, setNumberOfPeople] = useState(0);
 
   const form = useForm({
     initialValues: {
       nome: "",
-      sexo: "",
-      numeroCalcado: "",
-      data: Timestamp.fromDate(new Date()),
+      additionalPeople: "",
+      additionalNames: [],
+      data: new Date().toISOString().split('T')[0], // Set the initial value to today's date in 'YYYY-MM-DD' format
       confirmado: true,
     },
   });
 
   const handleSubmit = (values: typeof form.values) => {
-    saveData("guests", values);
+    const dataTimestamp = Timestamp.fromDate(new Date(values.data)); // Convert date string to Timestamp object
+    const updatedValues = { ...values, data: dataTimestamp };
+
+    saveData("guests", updatedValues);
+
+    const nome = values.nome;
+    const additionalPeople = values.additionalPeople;
+    const additionalNames = values.additionalNames.join(", ");
+    const data = new Date(values.data).toLocaleDateString('pt-BR'); // Convert the date string to a localized date string
+    const confirmado = values.confirmado ? "Sim" : "Não";
+
+    const message = `
+      *Nome:* ${nome}\n
+      *Quantas pessoas você vai levar juntos com você?:* ${additionalPeople}\n
+      *Nomes das pessoas que vão junto:* ${additionalNames}\n
+      *Data:* ${data}\n
+      *Confirmado:* ${confirmado}
+    `;
+
+    const url = `https://wa.me/61995777899?text=${encodeURIComponent(message)}`;
+
+    // Open WhatsApp URL
+    window.open(url, '_blank');
+
     form.reset();
-    setDisplayShowsSize(false);
+    setNumberOfPeople(0);
     setModalOpen(true);
   };
 
@@ -54,34 +77,44 @@ export default function GuestsConfirm() {
                 {...form.getInputProps("nome")}
               ></TextInput>
             </div>
-            <div className="flex flex-wrap justify-between w-3/5">
-              <div className="w-full md:w-[48%]">
-                <Select
+            <div className="w-3/5">
+              <Select
+                withAsterisk
+                label="Quantas pessoas você vai levar junto com você?"
+                placeholder="Selecione"
+                data={[
+                  { value: "0", label: "Nenhuma" },
+                  { value: "1", label: "1" },
+                  { value: "2", label: "2" },
+                  { value: "3", label: "3" },
+                  { value: "4", label: "4" },
+                  { value: "5", label: "5" },
+                ]}
+                {...form.getInputProps("additionalPeople")}
+                onChange={(value) => {
+                  form.setFieldValue("additionalPeople", value!);
+                  setNumberOfPeople(parseInt(value!));
+                }}
+              ></Select>
+            </div>
+            {Array.from({ length: numberOfPeople }, (_, index) => (
+              <div className="w-3/5" key={index}>
+                <TextInput
                   withAsterisk
-                  label="Sexo"
-                  placeholder="Selecione"
-                  data={[
-                    { value: "M", label: "Masculino" },
-                    { value: "F", label: "Feminino" },
-                  ]}
-                  {...form.getInputProps("sexo")}
-                  onChange={(event) => {
-                    // Set the value of the select
-                    form.setFieldValue("sexo", event!);
-                    setDisplayShowsSize(event === "F");
-                  }}
-                ></Select>
+                  label={`Nome da pessoa ${index + 1}`}
+                  placeholder={`Nome da pessoa ${index + 1}`}
+                  {...form.getInputProps(`additionalNames.${index}`)}
+                ></TextInput>
               </div>
-              <div className="w-full md:w-[48%] mt-4 md:mt-0">
-                {displayShowsSize && (
-                  <TextInput
-                    withAsterisk
-                    label="Numero do calçado"
-                    placeholder="Numeração para chinelo"
-                    {...form.getInputProps("numeroCalcado")}
-                  ></TextInput>
-                )}
-              </div>
+            ))}
+            <div className="w-3/5">
+              <TextInput
+                withAsterisk
+                label="Data"
+                placeholder="Data"
+                type="date"
+                {...form.getInputProps("data")}
+              ></TextInput>
             </div>
             <div className="pt-2">
               <button
